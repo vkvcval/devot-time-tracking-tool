efb85d08edea88a7d4517546e1684773f5a13cb4
Valentina Vukovic <valentininaemailadresa@gmail.com>
2023-07-29 22:43:21 +0200

    add icons export

---
+++ src/lib/icon.ts
@@ -0,0 +1,27 @@
+import TimeIcon from '@assets/icons/noun_Time_1856586 1.svg';
+import LeftIcon from '@assets/icons/item-caretleft.svg';
+import CalendarIcon from '@assets/icons/noun_daily calendar_4064786 1.svg';
+import EditIcon from '@assets/icons/noun_edit_1856545 1.svg';
+import HistoryIcon from '@assets/icons/noun_History_3323426 1.svg';
+import PauseIcon from '@assets/icons/noun_pause_1948188 2.svg';
+import PlayIcon from '@assets/icons/noun_play_1570735 1.svg';
+import StopIcon from '@assets/icons/noun_stop button_2729482 1.svg';
+import WatchIcon from '@assets/icons/noun_stop watch_2496084 1.svg';
+import TrashIcon from '@assets/icons/noun_Trash_322220 1.svg';
+import TurnOffIcon from '@assets/icons/noun_turn off_2785150 1.svg';
+import UserIcon from '@assets/icons/noun_User_1485280 1.svg';
+
+export {
+  TimeIcon,
+  LeftIcon,
+  CalendarIcon,
+  EditIcon,
+  HistoryIcon,
+  PauseIcon,
+  PlayIcon,
+  StopIcon,
+  WatchIcon,
+  TrashIcon,
+  TurnOffIcon,
+  UserIcon,
+};
