b5053cf8bf40767394105df38f2965393ccd6de0
Valentina Vukovic <valentininaemailadresa@gmail.com>
2023-07-30 11:06:07 +0200

    add toast message functionality

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,24 @@
 import '@styles/globals.scss';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
 
 import type { AppProps } from 'next/app';
+import { useRef } from 'react';
 import { AuthContextProvider } from '@/context/AuthContext';
+import { Toast, ToastMessage } from 'primereact/toast';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const toast = useRef<Toast>(null);
+
+  const showToastMessage = (message: ToastMessage) => {
+    toast.current?.show(message);
+  };
+
   return (
     <AuthContextProvider>
       <div>
-        <Component {...pageProps} />
+        <Toast ref={toast} position='bottom-right' />
+        <Component {...pageProps} showToastMessage={showToastMessage} />
       </div>
     </AuthContextProvider>
   );
